24083243083d8ab00f3285fff39972d50599a261
andrianggoro21 <andri.anggoro21@gmail.com>
2023-10-02 14:31:19 +0700

    create stopwatch and home

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,24 @@
+import { Box } from '@chakra-ui/react';
+import { Link } from '@chakra-ui/react';
+import './style.css'
+
+const Navbar = () => {
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+          x.className += " responsive";
+        } else {
+          x.className = "topnav";
+        }
+      }
+    return (
+        <Box id='myTopnav' className='topnav' backgroundColor='#000000'>
+            <Box><Link>Kelompok 2</Link></Box>
+                <Link className='link' href='/'>Home</Link>
+                <Link className='link'>Counter</Link>
+                <Link className='link' href='/soal2'>Stopwatch</Link>
+                <Link className='icon'onClick={myFunction} ><i className="fa fa-bars"/></Link>
+        </Box>
+    )
+};
+export default Navbar;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../../components/navbar";
+import { Box } from '@chakra-ui/react';
+import { Heading } from '@chakra-ui/react';
+const Home = () => {
+    return (
+        <>
+            <Navbar/>
+            <Box maxW='100vw' minH='100vh' backgroundColor='#000000' display='flex' justifyContent='center' paddingTop='300px'>
+                <Heading color='#ffffff' textAlign='center'>Welcome to Exercise 7 <br/> Module Front Web Development</Heading>
+            </Box>
+        </>
+        
+        
+    )
+
+}
+export default Home;
