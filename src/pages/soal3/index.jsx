d2c404889d68ef7de2e9ab1926e2cd4877ec2149
Rizky Noor Ichwanudin <rizky.noor.ichwanudin@gmail.com>
2023-10-02 10:58:50 +0700

    first-commit

---
+++ src/pages/soal3/index.jsx
@@ -0,0 +1,15 @@
+import { Box, Text, FormControl, FormLabel, Input } from "@chakra-ui/react";
+
+const Soal3 = () => {
+  return (
+    <Box bg="" h="100vh" w="100vw">
+      <Text>Halo</Text>
+      <FormControl>
+        <FormLabel>First name</FormLabel>
+        <Input placeholder="Ketik Untuk Mencari Data" />
+      </FormControl>
+    </Box>
+  );
+};
+
+export default Soal3;
