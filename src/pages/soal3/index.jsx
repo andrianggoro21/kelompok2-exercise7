77c7f28f9f40f286d419a30e912752bb16cfc189
Rizky Noor Ichwanudin <rizky.noor.ichwanudin@gmail.com>
2023-10-02 15:27:14 +0700

    Update Soal3

--- src/pages/soal3/index.jsx
+++ src/pages/soal3/index.jsx
@@ -1,13 +1,75 @@
-import { Box, Text, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  List,
+  Button,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 const Soal3 = () => {
+  const dummy = [
+    "Banana",
+    "Apple",
+    "Orange",
+    "Mango",
+    "Pineapple",
+    "Watermelon",
+    "Strawberry",
+    "Persimmon",
+  ];
+  const [searchDummy, setSearchDummy] = useState(dummy);
+
+  const filterDummy = (event) => {
+    const query = event.target.value;
+    let updateList = [...dummy];
+    updateList = updateList.filter((item) => {
+      return item.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+    });
+    setSearchDummy(updateList);
+  };
+
   return (
-    <Box bg="" h="100vh" w="100vw">
-      <Text>Halo</Text>
-      <FormControl>
-        <FormLabel>First name</FormLabel>
-        <Input placeholder="Ketik Untuk Mencari Data" />
-      </FormControl>
+    <Box h="100vh" w="100vw" align="center" mt="40px" fontFamily="roboto">
+      <Box
+        bgGradient="linear(to-br, yellow.200, red, purple.600)"
+        p="1em"
+        w="700px"
+        borderRadius="10px"
+      >
+        <FormControl
+          onChange={filterDummy}
+          flexDirection="row"
+          display="flex"
+          justifyContent="center"
+        >
+          <FormLabel fontWeight="medium" fontSize="19px">
+            Search :{" "}
+          </FormLabel>
+          <Input
+            placeholder="Ketik Untuk Mencari Data"
+            h="30px"
+            w="310px"
+            bg="white"
+          />
+        </FormControl>
+        <Box p="10px">
+          {searchDummy.map((item, index) => (
+            <List p="10px">
+              <Button
+                key={index}
+                h="23px"
+                w="125px"
+                fontWeight="light"
+                fontSize="19px"
+              >
+                {item}
+              </Button>
+            </List>
+          ))}
+        </Box>
+      </Box>
     </Box>
   );
 };
